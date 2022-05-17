d4cd1c5cb854bdac4a048f6905688627d9b4dcd8
abhishek-phukon-borah <105643080+abhishek-phukon-borah@users.noreply.github.com>
2022-05-17 20:14:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,12 +21,7 @@ let isRandom = false;
 let updateTimer;
 
 
-var viewMode = getCookie("view-mode"); 
-if(viewMode == "desktop"){ 
-    viewport.setAttribute('content', 'width=1024'); 
-}else if (viewMode == "mobile"){ 
-    viewport.setAttribute('content', 'width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no'); 
-} 
+ 
 
 const music_list = [
     
